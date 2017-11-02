565863136730bf3075189546e58e56ee0b3670de
tumaolin94 <524561223@qq.com>
2017-11-02 03:16:28 -0700

    add autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgForm} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import { OnInit } from '@angular/core';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sybol = 'Hello World!';
+
+  symbol_text;
+  form = new FormGroup({
+    symbol: new FormControl('', [
+      Validators.required,
+    ]),
+  });
+  get symbol(): any { return this.form.get('symbol'); }
+  filteredStates: Observable<any[]>;
+
+  states: any[] = [
+    {
+      name: 'Arkansas',
+    },
+    {
+      name: 'California',
+    },
+    {
+      name: 'Florida',
+    },
+    {
+      name: 'Texas',
+    }
+  ];
+
+  constructor() {
+    this.filteredStates = this.symbol.valueChanges
+      .startWith(null)
+      .map(state => state ? this.filterStates(state) : this.states.slice());
+  }
+
+  filterStates(name: string) {
+    return this.states.filter(state =>
+      state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
